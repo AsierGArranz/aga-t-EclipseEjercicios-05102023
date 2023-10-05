804d7a3db46097b5a823fd3d93d2fead77f13757
Asier González Arranz <asiergarranz@gmail.com>
2023-10-05 11:09:16 +0200

    push

---
+++ ACT1/ComentarioLinea/src/main/mainapp.java
@@ -0,0 +1,10 @@
+package main;
+
+public class mainapp {
+
+	public static void main(String args[]){
+		System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
---
+++ ACT3/ComentarioVariasLineas/src/main/mainapp.java
@@ -0,0 +1,14 @@
+package main;
+
+public class mainapp {
+
+	public static void main(String[] args) {
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+		for(int i=0; i<6; i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
